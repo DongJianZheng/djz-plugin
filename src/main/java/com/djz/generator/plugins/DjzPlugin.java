3b335cf2460bcbf211df796b855a868d4a44fe2d
dongjz <1175639137@qq.com>
2020-03-28 21:22:25 +0800

    我的插件相关的项目

---
+++ src/main/java/com/djz/generator/plugins/DjzPlugin.java
@@ -0,0 +1,39 @@
+package com.djz.generator.plugins;
+
+import java.util.List;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+
+/**
+ * 自定义插件
+ */
+public class DjzPlugin extends PluginAdapter {
+
+	public boolean validate(List<String> warnings) {
+		return true;
+	}
+	
+	@Override
+	public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+		AbstractXmlElementGenerator elementGenerator = new DjzAbstractXmlElementGenerator();
+		elementGenerator.setContext(context);
+		elementGenerator.setIntrospectedTable(introspectedTable);
+		elementGenerator.addElements(document.getRootElement());
+		return super.sqlMapDocumentGenerated(document, introspectedTable);
+	}
+
+	@Override
+	public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+		AbstractJavaMapperMethodGenerator methodGenerator = new DjzJavaMapperMethodGenerator();
+		methodGenerator.setContext(context);
+		methodGenerator.setIntrospectedTable(introspectedTable);
+		methodGenerator.addInterfaceElements(interfaze);
+		return super.clientGenerated(interfaze, topLevelClass, introspectedTable);
+	}
+}
