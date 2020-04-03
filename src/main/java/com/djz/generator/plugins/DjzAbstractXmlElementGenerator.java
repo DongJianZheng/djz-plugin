65534222863284dc4eac060a1d72198df4d0c895
Dongjianzheng <1175639137@qq.com>
2020-04-03 21:04:09 +0800

    修改

--- src/main/java/com/djz/generator/plugins/DjzAbstractXmlElementGenerator.java
+++ src/main/java/com/djz/generator/plugins/DjzAbstractXmlElementGenerator.java
@@ -1,11 +1,13 @@
 package com.djz.generator.plugins;
 
 import org.mybatis.generator.api.IntrospectedColumn;
-import org.mybatis.generator.api.dom.xml.Attribute;
-import org.mybatis.generator.api.dom.xml.TextElement;
-import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.xml.*;
 import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
 import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+
+import java.util.List;
 
 
 /**
@@ -15,14 +17,24 @@ public class DjzAbstractXmlElementGenerator extends AbstractXmlElementGenerator 
 
 	@Override
 	public void addElements(XmlElement parentElement) {
+		addBatchInsertSelectiveXml(parentElement);
+        addBatchUpdateXml(parentElement);
+		addBatchDeleteXml(parentElement);
+		addPageListXml(parentElement);
+
+
+	}
+
+	private void addPageListXml(XmlElement parentElement) {
 		// 增加All_COLUMNS_QUERY
 		XmlElement sql = new XmlElement("sql");
 		sql.addAttribute(new Attribute("id", "All_COLUMNS_QUERY"));
 		//在这里添加where条件
-        XmlElement selectTrimElement = new XmlElement("trim"); //设置trim标签
-        selectTrimElement.addAttribute(new Attribute("prefix", "WHERE"));  
-        selectTrimElement.addAttribute(new Attribute("prefixOverrides", "AND | OR")); //添加where和and
+		XmlElement selectTrimElement = new XmlElement("trim"); //设置trim标签
+		selectTrimElement.addAttribute(new Attribute("prefix", "WHERE"));
+		selectTrimElement.addAttribute(new Attribute("prefixOverrides", "AND | OR")); //添加where和and
 		StringBuilder sb = new StringBuilder();
+		StringBuilder allBaseColumnsb = new StringBuilder();
 		for(IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
 			XmlElement selectNotNullElement = new XmlElement("if"); //$NON-NLS-1$
 			sb.setLength(0);
@@ -35,57 +47,181 @@ public class DjzAbstractXmlElementGenerator extends AbstractXmlElementGenerator 
 			// 添加别名t
 
 			sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+			allBaseColumnsb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+			allBaseColumnsb.append(",");
 			// 添加等号
 			sb.append(" = ");
 			sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
-            selectNotNullElement.addElement(new TextElement(sb.toString()));
-            selectTrimElement.addElement(selectNotNullElement);
+			selectNotNullElement.addElement(new TextElement(sb.toString()));
+			selectTrimElement.addElement(selectNotNullElement);
 		}
 		sql.addElement(selectTrimElement);
 		parentElement.addElement(sql);
 
-		// 公用include
-		XmlElement selecAllColumns = new XmlElement("include");
-		selecAllColumns.addAttribute(new Attribute("refid", "Base_Column_List"));
+
+
+		if(!parentElement.getFormattedContent(0).contains("Base_Column_List")){
+			// 公用include
+			XmlElement include = new XmlElement("sql");
+			include.addAttribute(new Attribute("id", "Base_Column_List"));
+			include.addElement(new TextElement(allBaseColumnsb.toString().substring(0,allBaseColumnsb.length()-1)));
+			parentElement.addElement(include);
+
+		}
+
 		// 公用select
 		sb.setLength(0);
-		sb.append("select ");
-		sb.append(" * ");
+		sb.append("select");
+		sb.append(" <include refid=\"Base_Column_List\" />  ");
 		sb.append("from ");
 		sb.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
 		sb.append(" ");
 		TextElement selectText = new TextElement(sb.toString());
-		
+
 		// 公用include
 		XmlElement include = new XmlElement("include");
 		include.addAttribute(new Attribute("refid", "All_COLUMNS_QUERY"));
-
-		// 增加find
-		XmlElement find = new XmlElement("select");
-		find.addAttribute(new Attribute("id", "find"));
-		find.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		find.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		find.addElement(selectText);
-		find.addElement(include);
-		parentElement.addElement(find);
-		
-		// 增加list
-		XmlElement list = new XmlElement("select");
-		list.addAttribute(new Attribute("id", "list"));
-		list.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		list.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		list.addElement(selectText);
-		list.addElement(include);
-		parentElement.addElement(list);
-		
 		// 增加pageList
 		XmlElement pageList = new XmlElement("select");
-		pageList.addAttribute(new Attribute("id", "pageList"));
+		pageList.addAttribute(new Attribute("id", "selectList"));
 		pageList.addAttribute(new Attribute("resultMap", "BaseResultMap"));
 		pageList.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
 		pageList.addElement(selectText);
 		pageList.addElement(include);
 		parentElement.addElement(pageList);
+	}
+
+	public void addBatchInsertSelectiveXml(XmlElement parentElement) {
+		List<IntrospectedColumn> columns = introspectedTable.getAllColumns();
+
+		String incrementField = introspectedTable.getTableConfiguration().getProperties().getProperty("incrementField");
+		if (incrementField != null) {
+			incrementField = incrementField.toUpperCase();
+		}
+		XmlElement javaPropertyAndDbType = new XmlElement("trim");
+		javaPropertyAndDbType.addAttribute(new Attribute("prefix", " ("));
+		javaPropertyAndDbType.addAttribute(new Attribute("suffix", ")"));
+		javaPropertyAndDbType.addAttribute(new Attribute("suffixOverrides", ","));
+
+		XmlElement insertBatchElement = new XmlElement("insert");
+		insertBatchElement.addAttribute(new Attribute("id", "insertBatchSelective"));
+		insertBatchElement.addAttribute(new Attribute("parameterType", "java.util.List"));
+
+		XmlElement trim1Element = new XmlElement("trim");
+		trim1Element.addAttribute(new Attribute("prefix", "("));
+		trim1Element.addAttribute(new Attribute("suffix", ")"));
+		trim1Element.addAttribute(new Attribute("suffixOverrides", ","));
+		for (IntrospectedColumn introspectedColumn : columns) {
+			String columnName = introspectedColumn.getActualColumnName();
+			if (!columnName.toUpperCase().equals(incrementField)) {
+				XmlElement iftest = new XmlElement("if");
+				iftest.addAttribute(new Attribute("test", "list[0]." + introspectedColumn.getJavaProperty() + "!=null"));
+				iftest.addElement(new TextElement(columnName + ","));
+				trim1Element.addElement(iftest);
+				XmlElement trimiftest = new XmlElement("if");
+				trimiftest.addAttribute(new Attribute("test", "item." + introspectedColumn.getJavaProperty() + "!=null"));
+				trimiftest.addElement(new TextElement("#{item." + introspectedColumn.getJavaProperty() + ",jdbcType=" + introspectedColumn.getJdbcTypeName() + "},"));
+				javaPropertyAndDbType.addElement(trimiftest);
+			}
+		}
+
+		XmlElement foreachElement = new XmlElement("foreach");
+		foreachElement.addAttribute(new Attribute("collection", "list"));
+		foreachElement.addAttribute(new Attribute("index", "index"));
+		foreachElement.addAttribute(new Attribute("item", "item"));
+		foreachElement.addAttribute(new Attribute("separator", ","));
+		insertBatchElement.addElement(new TextElement("insert into " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+		insertBatchElement.addElement(trim1Element);
+		insertBatchElement.addElement(new TextElement(" values "));
+		foreachElement.addElement(javaPropertyAndDbType);
+		insertBatchElement.addElement(foreachElement);
+
+		parentElement.addElement(insertBatchElement);
+	}
+
+    public void addBatchUpdateXml(XmlElement parentElement) {
+        List<IntrospectedColumn>  columns = introspectedTable.getAllColumns();
+		String keyColumn = "";
+		try {
+			keyColumn = ((IntrospectedColumn)introspectedTable.getPrimaryKeyColumns().get(0)).getActualColumnName();
+		}catch (Exception e){
+        	return;
+		}
+
+
+        XmlElement insertBatchElement = new XmlElement("update");
+        insertBatchElement.addAttribute(new Attribute("id", "updateBatchByPrimaryKeySelective"));
+        insertBatchElement.addAttribute(new Attribute("parameterType", "java.util.List"));
+
+        XmlElement foreach = new XmlElement("foreach");
+        foreach.addAttribute(new Attribute("collection", "list"));
+        foreach.addAttribute(new Attribute("item", "item"));
+        foreach.addAttribute(new Attribute("index", "index"));
+        foreach.addAttribute(new Attribute("separator", ";"));
+
+        foreach.addElement(new TextElement("update " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+
+        XmlElement trim1Element = new XmlElement("set");
+        String columnName;
+        for (IntrospectedColumn introspectedColumn : columns) {
+            columnName = introspectedColumn.getActualColumnName();
+            if (!columnName.toUpperCase().equalsIgnoreCase(keyColumn)) {
+                XmlElement ifxml = new XmlElement("if");
+                ifxml.addAttribute(new Attribute("test", "item." + introspectedColumn.getJavaProperty() + "!=null"));
+                ifxml.addElement(new TextElement(columnName + "=#{item." + introspectedColumn.getJavaProperty() + ",jdbcType=" + introspectedColumn.getJdbcTypeName() + "},"));
+                trim1Element.addElement(ifxml);
+            }
+        }
+        foreach.addElement(trim1Element);
+
+        foreach.addElement(new TextElement("where "));
+        int index = 0;
+        for (IntrospectedColumn i : introspectedTable.getPrimaryKeyColumns()) {
+            foreach.addElement(new TextElement((index > 0 ? " AND " : "") + i.getActualColumnName() + " = #{item." + i.getJavaProperty() + ",jdbcType=" + i.getJdbcTypeName() + "}"));
+        }
+
+        insertBatchElement.addElement(foreach);
+
+        parentElement.addElement(insertBatchElement);
+    }
+
+	/**
+	 * 批量删除的xml方法生成
+	 * @param parentElement
+	 */
+	private void addBatchDeleteXml(XmlElement parentElement){
+		String tableName = introspectedTable.getFullyQualifiedTableNameAtRuntime();
+
+		String key = "";
+		try {
+			key = ((IntrospectedColumn)introspectedTable.getPrimaryKeyColumns().get(0)).getActualColumnName();
+		}catch (Exception e){
+			return;
+		}
+		String baseSql = "delete from "+ introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()+" where  "+key+" in (";
+
+		FullyQualifiedJavaType paramType = introspectedTable.getPrimaryKeyColumns().get(0).getFullyQualifiedJavaType();
+		XmlElement deleteElement = new XmlElement("delete");
+		deleteElement.addAttribute(new Attribute("id", "deleteBatchByKeys"));
+		deleteElement.addAttribute(new Attribute("parameterType", "java.util.List"));
+
+		XmlElement foreach = new XmlElement("foreach");
+		foreach.addAttribute(new Attribute("collection", "list"));
+		foreach.addAttribute(new Attribute("item", "item"));
+		foreach.addAttribute(new Attribute("index", "index"));
+		foreach.addAttribute(new Attribute("separator", ","));
+		foreach.addElement(new TextElement("#{item}"));
+
+
+		deleteElement.addElement(new TextElement(baseSql));
+
+
+
+		deleteElement.addElement(foreach);
+
+		deleteElement.addElement(new TextElement(")"));
+
+		parentElement.addElement(deleteElement);
 	}
 
 }
--- src/main/java/com/djz/generator/plugins/DjzJavaMapperMethodGenerator.java
+++ src/main/java/com/djz/generator/plugins/DjzJavaMapperMethodGenerator.java
@@ -9,87 +9,135 @@ import org.mybatis.generator.api.dom.java.JavaVisibility;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+import org.mybatis.generator.internal.util.messages.Messages;
 
 public class DjzJavaMapperMethodGenerator extends AbstractJavaMapperMethodGenerator {
 
 	@Override
 	public void addInterfaceElements(Interface interfaze) {
-		
-		addInterfaceFind(interfaze);
-		addInterfaceList(interfaze);
-		addInterfacePageList(interfaze);
-	}
-	
-	private void addInterfaceFind(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
-		// 添加Lsit的包
-		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
-		// 创建方法对象
-		Method method = new Method();
-		// 设置该方法为public
-		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// 方法对象设置返回类型对象
-		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("find");
 
-		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
-		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
-		addMapperAnnotations(interfaze, method);
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
+		addBatchInsertMethod(interfaze);
+		addBatchUpdateMethod(interfaze);
+		addBatchDeleteMethod(interfaze);
+	}
+
+
+	private void addBatchInsertMethod(Interface interfaze) {
+		Set importedTypes = new TreeSet();
+		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
+		importedTypes.add(new FullyQualifiedJavaType(introspectedTable.getBaseRecordType()));
+
+		Method ibsmethod = new Method();
+
+		ibsmethod.setVisibility(JavaVisibility.PUBLIC);
+
+		FullyQualifiedJavaType ibsreturnType = FullyQualifiedJavaType.getIntInstance();
+		ibsmethod.setReturnType(ibsreturnType);
+
+		ibsmethod.setName("insertBatchSelective");
+
+		FullyQualifiedJavaType paramType = FullyQualifiedJavaType.getNewListInstance();
+		FullyQualifiedJavaType paramListType;
+		if (introspectedTable.getRules().generateBaseRecordClass()) {
+			paramListType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+		}
+		else
+		{
+			if (introspectedTable.getRules().generatePrimaryKeyClass())
+				paramListType = new FullyQualifiedJavaType(introspectedTable.getPrimaryKeyType());
+			else
+				throw new RuntimeException(Messages.getString("RuntimeError.12"));
+		}
+
+		paramType.addTypeArgument(paramListType);
+
+		ibsmethod.addParameter(new Parameter(paramType, "records"));
+
+		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(ibsmethod, interfaze, introspectedTable)) {
 			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
+			interfaze.addMethod(ibsmethod);
+		}
+
+
+	}
+
+
+	private void addBatchUpdateMethod(Interface interfaze) {
+		Set importedTypes = new TreeSet();
+		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
+		importedTypes.add(new FullyQualifiedJavaType(introspectedTable.getBaseRecordType()));
+
+		Method ibsmethod = new Method();
+
+		ibsmethod.setVisibility(JavaVisibility.PUBLIC);
+
+		FullyQualifiedJavaType ibsreturnType = FullyQualifiedJavaType.getIntInstance();
+		ibsmethod.setReturnType(ibsreturnType);
+
+		ibsmethod.setName("updateBatchByPrimaryKeySelective");
+
+		FullyQualifiedJavaType paramType = FullyQualifiedJavaType.getNewListInstance();
+		FullyQualifiedJavaType paramListType;
+		if (introspectedTable.getRules().generateBaseRecordClass()) {
+			paramListType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+		}
+		else
+		{
+
+			if (introspectedTable.getRules().generatePrimaryKeyClass())
+				paramListType = new FullyQualifiedJavaType(introspectedTable.getPrimaryKeyType());
+			else
+				throw new RuntimeException(Messages.getString("RuntimeError.12"));
+		}
+
+		paramType.addTypeArgument(paramListType);
+
+		ibsmethod.addParameter(new Parameter(paramType, "list"));
+
+		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(ibsmethod, interfaze, introspectedTable)) {
+			interfaze.addImportedTypes(importedTypes);
+			interfaze.addMethod(ibsmethod);
 		}
 	}
-	
-	private void addInterfaceList(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
-		// 添加Lsit的包
-		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
-		// 创建方法对象
-		Method method = new Method();
-		// 设置该方法为public
-		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = FullyQualifiedJavaType.getNewListInstance();
-		FullyQualifiedJavaType listType;
-		// 设置List的类型是实体类的对象
-		listType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		importedTypes.add(listType);
-		// 返回类型对象设置为List
-		returnType.addTypeArgument(listType);
-		// 方法对象设置返回类型对象
-		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("list");
 
-		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
-		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
-		addMapperAnnotations(interfaze, method);
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
+	private void addBatchDeleteMethod(Interface interfaze) {
+		Set importedTypes = new TreeSet();
+		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
+		importedTypes.add(new FullyQualifiedJavaType(introspectedTable.getBaseRecordType()));
+
+		Method ibsmethod = new Method();
+
+		ibsmethod.setVisibility(JavaVisibility.PUBLIC);
+
+		FullyQualifiedJavaType ibsreturnType = FullyQualifiedJavaType.getIntInstance();
+		ibsmethod.setReturnType(ibsreturnType);
+
+		ibsmethod.setName("deleteBatchByKeys");
+
+		FullyQualifiedJavaType paramType = FullyQualifiedJavaType.getNewListInstance();
+		FullyQualifiedJavaType paramListType;
+		if (introspectedTable.getRules().generateBaseRecordClass()) {
+			paramListType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+		}
+		else
+		{
+
+			if (introspectedTable.getRules().generatePrimaryKeyClass())
+				paramListType = new FullyQualifiedJavaType(introspectedTable.getPrimaryKeyType());
+			else
+				throw new RuntimeException(Messages.getString("RuntimeError.12"));
+		}
+
+		paramType.addTypeArgument(paramListType);
+
+		ibsmethod.addParameter(new Parameter(paramType, "list"));
+
+		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(ibsmethod, interfaze, introspectedTable)) {
 			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
+			interfaze.addMethod(ibsmethod);
 		}
 	}
-	
+
 	private void addInterfacePageList(Interface interfaze) {
 		// 先创建import对象
 		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
@@ -110,7 +158,7 @@ public class DjzJavaMapperMethodGenerator extends AbstractJavaMapperMethodGenera
 		// 方法对象设置返回类型对象
 		method.setReturnType(returnType);
 		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("pageList");
+		method.setName("selectList");
 
 		// 设置参数类型是对象
 		FullyQualifiedJavaType parameterType;
@@ -120,7 +168,7 @@ public class DjzJavaMapperMethodGenerator extends AbstractJavaMapperMethodGenera
 		// 为方法添加参数，变量名称record
 		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
 		//
-		addMapperAnnotations(interfaze, method);
+
 		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
 		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
 			interfaze.addImportedTypes(importedTypes);
@@ -128,6 +176,5 @@ public class DjzJavaMapperMethodGenerator extends AbstractJavaMapperMethodGenera
 		}
 	}
 
-	public void addMapperAnnotations(Interface interfaze, Method method) {
-	}
+
 }
